4b117c12eb1d39c0322411c04c61d410196d05ea
n0th1ng999 <tiagogabrielpereira@gmail.com>
2023-02-04 21:08:27 +0000

    v.0.0
    Setup

---
+++ Adriano-App/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// plugins/vuetify.js
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+export default defineNuxtConfig({
+
+})
+
